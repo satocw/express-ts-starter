e5fcf2f9de6fc27f4944f9bd0996767113c5d194
NISHI Satoshi <nishi@flexsche.com>
2019-03-14 10:53:22 +0900

    query

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,8 @@ import { Request, Response } from "express";
 export const GetIndex = (req: Request, res: Response) => {
   res.send("Get Index OK");
 };
+
+export const GetQuery = (req: Request, res: Response) => {
+  const params = req.query;
+  res.send("Get Query: " + JSON.stringify(params));
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
-import { GetIndex } from "./controllers/index";
+import { GetIndex, GetQuery } from "./controllers/index";
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", GetIndex);
+app.get("/query", GetQuery);
 
 app.listen(PORT, () => {
     console.log(`App is running at port ${PORT}`);
